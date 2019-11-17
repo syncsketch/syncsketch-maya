af6cead92e91b0d2aa7b8cc30139c6746642858a
yafes <yafesdev@gmail.com>
2019-11-16 22:28:29 -0800

    set's useExpiringToken to True, as otherwise the server won't accept the request

--- syncsketchGUI/lib/user.py
+++ syncsketchGUI/lib/user.py
@@ -153,7 +153,7 @@ class SyncSketchUser():
             #     self.get_name(), self.get_api_key(), self.api_host,))
             self.host_data = syncsketch.SyncSketchAPI(self.get_name(),
                                                        self.get_api_key(),
-                                                       useExpiringToken=False,
+                                                       useExpiringToken=True,
                                                        host = self.api_host,
                                                        debug=False)
             return self.host_data
