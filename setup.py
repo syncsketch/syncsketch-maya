d4ea3609187fba7a0a4fdadbdb29a630e9f39cc5
yafes <yafesdev@gmail.com>
2019-11-17 13:13:27 -0800

    bump version to v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'SyncsketchGUI',
-    version = '1.0.9',
+    version = '1.1.0',
     url = 'https://github.com/syncsketch/syncsketch-maya.git',
     author = 'Syncsketch',
     author_email = "dev@syncsketch.com",
