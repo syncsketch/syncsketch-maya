86febc7cd1e16322967cc4bf5483cb6a08f2f69b
Janik <40320923+jo023@users.noreply.github.com>
2020-09-03 10:25:44 +0200

    makes install script maker more compatible

--- syncsketchGUI/installScripts/makeMelRunner.py
+++ syncsketchGUI/installScripts/makeMelRunner.py
@@ -9,7 +9,15 @@ with open(pythonInstallerPath, "r") as f:
 lines.insert(0, "python(\"\n")
 newText = ""
 for line in lines:
-    newText += line.strip("\n") + "\\n\\\n"
+    '''
+    The old way or alternative way is:
+        newText += line.strip("\n") + "\\n\\\n"
+    The last \\\n makes the final mel file more human readable but introduces 
+    some issues, since Windows encodes this as CRLF (\r\n) which is not compatible on 
+    Unix systems. 
+    Removing the last \\\n makes it less readable but more compatible, which is preferred.
+    '''
+    newText += line.strip("\n") + "\\n"
     print(line)
 
 newText += "\");"
