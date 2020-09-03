fbdbbd7d40efa2d140fef94481b9455408962d5d
Janik Otterbach <you@janikotterbach@gmail.com>
2020-09-03 15:33:08 +0200

    add documentation to web module

--- syncsketchGUI/lib/gui/syncsketchWidgets/web/__init__.py
+++ syncsketchGUI/lib/gui/syncsketchWidgets/web/__init__.py
@@ -1,7 +1,13 @@
 import logging
 logger = logging.getLogger("syncsketchGUI")
 
-
+'''
+The Pyside2 version used in Maya2017 doesnt support the new QWebEngineView yet. 
+The Pyside2 version used in Maya2020 dropped the support of the old QWebView widget. 
+In order to support both Maya versions this module switches between two widget implementations. 
+Unfortunately Maya2019 seems to have a bug with the QWebEngineView widget, since it displays only a grey window.
+Therefore the old widget is used as default. With this implementation only >=Maya2020 uses the new widgets. 
+'''
 try: 
     from webLoginWidget import WebLoginWindow as LoginView
     from webLoginWidget import logout_view
