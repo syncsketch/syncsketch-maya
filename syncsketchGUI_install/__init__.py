16e9949478345f91d56ac93fffba8a18c0d8b5f2
Janik Otterbach <janikotterbach@gmail.com>
2021-02-08 15:59:40 +0100

    add installer modules

---
+++ syncsketchGUI_install/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger('syncsketchGUI_install')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s - %(message)s]', "%Y-%m-%d %H:%M:%S")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
