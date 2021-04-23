9b4e269b86e9d34694821032f6acbe43816fd4eb
Janik Otterbach <you@janikotterbach@gmail.com>
2021-04-23 12:24:14 +0200

    tmp version tag for easier dev install

--- syncsketchGUI/installScripts/installGui.py
+++ syncsketchGUI/installScripts/installGui.py
@@ -33,6 +33,7 @@ else:
     from PySide.QtCore import *
     from PySide.QtGui import *
 versionTag = os.getenv('SS_DEV') or 'release'
+versionTag = "dev" #FIXME 
 SYNCSKETCH_GUI_RELEASE_PATH = 'https://github.com/syncsketch/syncsketch-maya/archive/{}.zip'.format(versionTag)
 SYNCSKETCH_API_RELEASE_PATH = 'https://github.com/syncsketch/python-api/archive/v1.0.7.8.zip'
 
